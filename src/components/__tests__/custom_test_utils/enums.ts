65288d5c0c0a662d2a15ddde7906a594b2504213
gyuudon3187 <ddahlb@kth.se>
2024-02-01 09:24:51 +0100

    Yet another missing change to the documentation (see last commit)

--- src/components/__tests__/custom_test_utils/enums.ts
+++ src/components/__tests__/custom_test_utils/enums.ts
@@ -1,7 +1,7 @@
 /** 
  * FILE DESCRIPTION:
- * The enums in this file are meant to be imported to test files for more consistent adherence to naming conventions. 
- * Resort to custom strings when the component, verb or time cannot be described by any of the enums below.
+ * The enums in this file are meant to be imported to test files to enforce naming conventions. 
+ * Resort to custom strings when the component, action or time cannot be described by any of the enums below.
  */
 
 /**
@@ -17,7 +17,7 @@ export enum GenericComponent {
 }
 
 /**
- * DESCRIPTION: The verb that is performed by a component or {@link GenericComponent}.
+ * DESCRIPTION: The action that is performed by a component or {@link GenericComponent}.
  * WHERE TO USE: Should be passed as the 1st argument to "vitest#it()" or "vitest#test()".
  * EXAMPLE: it(Action.HasEnglishTranslation, () => { ... })
  * RULES WHEN ADDING MORE PROPERTIES: First letter should be lowercase.
