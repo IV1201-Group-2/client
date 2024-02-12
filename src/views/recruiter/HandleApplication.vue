94208dc5389f4b9a3654988e3ff832e047b661bc
gyuudon3187 <ddahlb@kth.se>
2024-02-12 22:11:42 +0100

    Added placeholder for accept/reject applications view

---
+++ src/views/recruiter/HandleApplication.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <main style="height: 30rem">
+        <div class="text-h3 text-center mb-10">Handle Application</div>
+    </main>
+</template>
