7a92e45db0410a64dff641a61574662b48b5bc10
Hannes Mann <hannesmann2000@gmail.com>
2024-02-15 14:10:34 +0100

    Remove old test API from service

--- service/src/main/java/com/iv1201/clientservice/ServerController.java
+++ service/src/main/java/com/iv1201/clientservice/ServerController.java
@@ -9,40 +9,4 @@ public class ServerController {
     public String acceptConnection() {
         return "index.html";
     }
-
-    class Token {
-        private String value;
-
-        public Token(String value) {
-            this.value = value;
-        }
-
-        public String getValue() {
-            return value;
-        }
-    }
-
-    class Role {
-        private String value;
-
-        public Role(String value) {
-            this.value = value;
-        }
-
-        public String getValue() {
-            return value;
-        }
-    }
-
-    @GetMapping("/getToken")
-    @ResponseBody
-    public Token getToken() {
-        return new Token("test");
-    }
-
-    @PostMapping("/getRole")
-    @ResponseBody
-    public Role getRole(@RequestHeader("Authorization") String authorizationHeader) {
-        return authorizationHeader.substring("Bearer".length() + 1, authorizationHeader.length()).equals("test") ? new Role("Applicant") : new Role("Recruiter");
-    }
 }
